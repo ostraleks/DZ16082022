6acb6018b1ec3eff4883170bcfe148d674e1762d
aleksndr <ostraleks@gmail.com>
2022-08-16 22:18:34 +0500

    1.1

---
+++ 1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;
+for (int i = 1; i < B; i++)
+{result *= A;}
+Console.WriteLine($"А в степени В = {result}");
